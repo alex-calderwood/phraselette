9bf2c1352c40edaccf27c70cd636d15574f99ecf
Alex Calderwood <alexander.d.calderwood@gmail.com>
2024-07-01 14:27:57 -0400

    Delete unnecessary files

--- front-end/test.tsx
+++
@@ -1,7 +0,0 @@
-// Import React dependencies.
-import React, { useState } from 'react'
-// Import the Slate editor factory.
-import { createEditor } from 'slate'
-
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
