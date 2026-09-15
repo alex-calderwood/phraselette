6ab328c4e3026d049e58d506fc4e5e0151d23fea
Alex Calderwood <alexander.d.calderwood@gmail.com>
2026-09-14 20:11:35 -0700

    Remove old

--- old/back-end/network.py
+++
@@ -1,2 +0,0 @@
-# something unlikely to be seen, must match the client (in smarts.js)
-BREAK_TOKEN = "&&VE*A=]"
--- old/back-end/word_data.py
+++
@@ -1,6 +0,0 @@
-from phones import sound_out
-
-# Mapping between prism name and a function to grab that info
-get_additional_word_data = {
-    "sound": lambda token: sound_out(token["text"])
-}
